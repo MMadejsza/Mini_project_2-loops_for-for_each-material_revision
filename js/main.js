63fbce2470ff5245bc8b26bbae604550a4f96d8b
Maciej <maciejmad16@gmail.com>
2021-01-03 11:23:45 +0000

    adding every 5 li

--- js/main.js
+++ js/main.js
@@ -12,3 +12,29 @@ function btnAction() {
     });
     fnSize += 1;
 }
+
+let fnSize2 = 10;
+let btnSecond = document.querySelector(".second");
+btnSecond.style.fontSize2 = "20px";
+btnSecond.addEventListener("click", addList);
+
+function addList() {
+    let liArray2 = document.querySelectorAll("ul li");
+    for (let i = 1; i <= 5; i++) {
+        const li = createTag("li", "class", "liItem");
+        li.innerText = `Element${i}`;
+        const uList = document.querySelector(".list");
+        uList.appendChild(li);
+        li.style.fontSize = `${fnSize2++}px`;
+    }
+    liArray2.forEach(element => {
+        element.style.display = "block";
+    });
+
+}
+
+function createTag(tag, attribute, value) {
+    let element = document.createElement(tag);
+    element.setAttribute(attribute, value);
+    return element;
+}
